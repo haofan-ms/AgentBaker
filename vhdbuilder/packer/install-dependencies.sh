6bdae0c28ff073ab335d023aba1c3784c6dd9cb8
haofan-ms <hafa@microsoft.com>
2021-09-27 20:01:45 -0700

    update

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -64,7 +64,7 @@ if [[ ${CONTAINER_RUNTIME:-""} == "containerd" ]]; then
   installStandaloneContainerd
   echo "  - containerd v${CONTAINERD_VERSION}" >> ${VHD_LOGS_FILEPATH}
   CRICTL_VERSIONS=("1.19.0" "1.20.0")
-  for CRICTL_VERSION in ${CRICTL_VERSIONS}; do
+  for CRICTL_VERSION in ${CRICTL_VERSIONS[@]}; do
     downloadCrictl ${CRICTL_VERSION}
     echo "  - crictl version ${CRICTL_VERSION}" >> ${VHD_LOGS_FILEPATH}
   done
